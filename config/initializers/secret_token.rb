2f11751b471f17bd07c26669e6f9bb8da1a66c6a
Amit Gulati <amit@movenbank.com>
2013-04-06 13:04:00 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DwollaPayroll::Application.config.secret_token = '42fe9746eff61e84210684c199bb23efecca01c85ab23f4bda947ae2d288594f93643b6c19fa7e4d7b92bc0aa4079dfb6773fc6e5fd62b1c2dcb451a61d2ce58'
