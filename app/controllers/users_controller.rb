d38b234d9c063251857c94c07d7950f981e72e24
Amit Gulati <amit@movenbank.com>
2013-04-06 21:55:43 -0400

    refactored home view to static pages controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
 
-  def home
-
-  end
-
   def login
     redirect_to Dwolla::Client.oauth_authentication_url(request.protocol, request.host_with_port)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 DwollaPayroll::Application.routes.draw do
 
-  match "home" => "users#home"
   post "login" => "users#login"
   match "dashboard" => "users#dashboard"
 
   match "dwolla_oauth_callback" => "callbacks#dwolla_oauth"
 
-  root :to => "users#home"
+  root :to => "static#home"
 
 end
